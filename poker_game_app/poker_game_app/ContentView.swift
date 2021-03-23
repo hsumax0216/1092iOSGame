167651cc516843c9632da4e9c2b3bf3bd9a1ee62
hsumax0216 <b2626235@gmail.com>
2021-03-23 21:05:40 +0800

    cover ing

--- poker_game_app/poker_game_app/ContentView.swift
+++ poker_game_app/poker_game_app/ContentView.swift
@@ -7,20 +7,67 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomePage: View {
     var body: some View {
-        VStack{
-            Text("Hello, world!")
-                .padding()
-            Button( action:{
-                //let a=GeneratePokers()
-                //GamePlay(DECK: a, peoples: 1)
-                //print("fuck you xcode")
-                testfunc()
-                },label:{
-                    Text("test")
+        GeometryReader{
+            geometry in  ZStack{
+                    Image("poker_background")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(minWidth: 0,maxWidth: .infinity)
+                        .edgesIgnoringSafeArea(.all)
+                    VStack(alignment: .leading){
+                        Text("Hello, world!")
+                            .padding()
+
+
+                        Button( action:{
+                            //let a=GeneratePokers()
+                            //GamePlay(DECK: a, peoples: 1)
+                            //print("fuck you xcode")
+                            testfunc()
+                        },label:{
+                            Text("test")
+                        }
+                        )
+                    }
+                    HStack(alignment: .center) {
+                        ZStack(alignment:.bottom){
+                            Image("back")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 52.76, height: 80, alignment: .center)
+                                .rotationEffect(.degrees(90))
+
+                            Image("2_of_clubs")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 52.76, height: 80, alignment: .center)
+                                .rotationEffect(.degrees(90))
+                        }
+                        .padding(.trailing,10)
+                        Image("wood")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 200,height:600, alignment: .center)
+                            .clipped()
+                        //.edgesIgnoringSafeArea(.all)
+                        ZStack(alignment:.bottom){
+                            Image("back")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 52.76, height: 80, alignment: .center)
+                                .rotationEffect(.degrees(-90))
+                            Image("2_of_clubs")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 52.76, height: 80, alignment: .center)
+                                .rotationEffect(.degrees(-90))
+                        }
+                        .padding(.leading,10)
+                    }
                 }
-            )
+
         }
     }
 }
@@ -28,7 +75,49 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
+            HomePage()
         }
     }
 }
+
+
+//for test func
+// struct HomePage: View {
+//    @State private var showSecondPage=false
+//     var body: some View {
+//         VStack{
+//             Text("Hello, world!")
+//                 .padding()
+//             Button( action:{
+//                showSecondPage=true
+//                 },label:{
+//                     Text("button")
+//                 }
+//             ).fullScreenCover(isPresented: $showSecondPage, content: {
+//                secondpage(showSecondPage: $showSecondPage)
+//             })
+//         }
+//     }
+// }
+//struct secondpage: View {
+//    @Binding var showSecondPage:Bool
+//    var body: some View {
+//        ZStack {
+//        Color.yellow
+//        .edgesIgnoringSafeArea(.all)
+//        Text("Second View")
+//        }
+//        .overlay(
+//        Button(action: {
+//        showSecondPage = false
+//        }, label: {
+//        Image(systemName: "xmark.circle.fill")
+//        .resizable()
+//        .frame(width: 50, height: 50)
+//        .padding(20)
+//        }), alignment: .topTrailing)
+//    }
+//    
+//}
+
+ 
