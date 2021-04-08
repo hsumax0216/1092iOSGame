232c28406f4d938dfc135fe0184888d8d4be68c4
hsumax0216 <b2626235@gmail.com>
2021-04-08 16:55:54 +0800

    created HW03

---
+++ gesture-alphabetGame/gesture-alphabetGame/gesture_alphabetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gesture_alphabetGameApp.swift
+//  gesture-alphabetGame
+//
+//  Created by 徐浩恩 on 2021/4/8.
+//
+
+import SwiftUI
+
+@main
+struct gesture_alphabetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
