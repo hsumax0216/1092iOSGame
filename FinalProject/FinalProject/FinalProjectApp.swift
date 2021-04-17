372b961598873cf0fda0160fb6195e9737e2a3cb
hsumax0216 <b2626235@gmail.com>
2021-04-18 04:51:04 +0800

    create finalProject

---
+++ FinalProject/FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by 徐浩恩 on 2021/4/18.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
