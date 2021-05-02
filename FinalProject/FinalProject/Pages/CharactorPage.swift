1beaf41cf2fff339854689e6dcf717a2efd9e185
hsumax0216 <b2626235@gmail.com>
2021-05-02 19:02:09 +0800

    just save

--- FinalProject/FinalProject/Pages/CharactorPage.swift
+++ FinalProject/FinalProject/Pages/CharactorPage.swift
@@ -13,38 +13,79 @@ struct CharactorPage: View {
     @State private var email: String = ""
     @State private var name: String = ""
     @State private var money: String = ""
+    @State private var imageURL:String = ""
     var body: some View {
         let screenWidth:CGFloat = UIScreen.main.bounds.size.width
-        VStack{
-            Image(uiImage: userImage)
-                .resizable()
-                .scaledToFit()
-                .border(Color.black, width: 1)
-                .padding(10)
-            HStack{
+        ZStack{
+            VStack{
+                HStack{
+                    Button(action: {
+                        currentPage = Pages.CharactorPage
+                    }, label: {
+                        Image(systemName: "arrow.left")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.purple)
+                            .frame(width:40,height:40)
+                            .padding(.leading,15)
+                    })
+                    Spacer()
+                    Button(action: {
+                        let player = Player(name:name, imageURL:imageURL,email:email,money:0,regTime:Date.init())
+                        currentPage = Pages.ProfilePage
+                    }, label: {
+                        Image(systemName: "arrow.right")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.purple)
+                            .frame(width:45,height:45)
+                            .padding(.trailing,15)
+                    })
+                }
                 Spacer()
-                Text("name:")
-                TextField("Your Name", text: $name)
-                    .frame(width:screenWidth/2)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.trailing,screenWidth/5)
             }
-            HStack{
-                Spacer()
-                Text("email:")
-                TextField("Your Email", text: $email)
-                    .frame(width:screenWidth/2)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.trailing,screenWidth/5)
+            VStack{
+                Image(uiImage: userImage)
+                    .resizable()
+                    .scaledToFit()
+                    .border(Color.black, width: 1)
+                    .padding(10)
+                HStack{
+                    Spacer()
+                    Text("name:")
+                    TextField("Your Name", text: $name)
+                        .frame(width:screenWidth/2)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.trailing,screenWidth/5)
+                }
+                HStack{
+                    Spacer()
+                    Text("email:")
+                    TextField("Your Email", text: $email)
+                        .frame(width:screenWidth/2)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.trailing,screenWidth/5)
+                }
+                HStack{
+                    Spacer()
+                    Text("money:")
+                    TextField("Your Money", text: $money)
+                        .frame(width:screenWidth/2)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .padding(.trailing,screenWidth/5)
+                }
             }
-            HStack{
-                Spacer()
-                Text("money:")
-                TextField("Your Money", text: $money)
-                    .frame(width:screenWidth/2)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
-                    .padding(.trailing,screenWidth/5)
+        }
+        .onAppear{
+            uploadPhoto(image: userImage) { result in
+                switch result {
+                case .success(let url):
+                    print(url)
+                    imageURL = url
+                case .failure(let error):
+                   print(error)
+                }
             }
         }
     }
