96fa2b02c520ca3ca6c5619580c218f5ddd3882c
hsumax0216 <b2626235@gmail.com>
2021-05-19 04:08:10 +0800

    Monopoly-Game init,game-design & Auth-design separate.

---
+++ Monopoly-Game/Monopoly-Game/Models/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Monopoly-Game
+//
+//  Created by 徐浩哲 on 2021/5/19.
+//
+
+import Foundation
---
+++ Monopoly-Game/Monopoly-Game/Views/Monopoly_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monopoly_GameApp.swift
+//  Monopoly-Game
+//
+//  Created by 徐浩哲 on 2021/5/19.
+//
+
+import SwiftUI
+
+@main
+struct Monopoly_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
